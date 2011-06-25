cacf88d20851bd23a7aca027e29b22ae444039f6
dirk luesebrink <dirk.luesebrink@gmail.com>
2011-06-25 20:53:14 +0200

    password now read from commandline and date option

--- gmail-stats.rb
+++ gmail-stats.rb
@@ -3,18 +3,40 @@
 require 'rubygems'
 require 'yaml'
 require 'net/imap'
+require 'highline/import'
 require 'applix'
+
+def get_password prompt = 'enter password: '
+  ask(prompt) {|q| q.echo = '*'}
+end
+
+Defaults = {
+  :date => (Date.today - 1).strftime('%e-%b-%Y'), # yesterday
+}
 
 class M < Net::IMAP
   def initialize username, password
     super 'imap.gmail.com', '993', true
-    #@imap = Net::IMAP.new 'imap.gmail.com', '993', true
     @username, @password = username, password
     login username, password
   end
 
   def folders
     @folders = self.list '', '[Gmail]/%'
+  end
+
+  # returns mail UID list
+  def mails_in_folder_on_given_date folder, date
+    examine folder
+    #condition = (date.nil? && ['all'] || ['ON', date.strftime '%e-%b-%Y']
+    uids = uid_search ['ON', date.strftime('%e-%b-%Y')]
+    #puts "#{uids.size} mails in #{folder} on #{date_s}"
+  end
+
+  def mails_in_folder_before_given_date folder, date
+    examine folder
+    uids = uid_search ['BEFORE', date.strftime('%e-%b-%Y')]
+    #puts "#{uids.size} mails in #{folder} on #{date_s}"
   end
 end
 
@@ -45,15 +67,18 @@ def dump_fetch_data fd
 end
 
 
-def number_of_mail_in_starred mbox
-  folder = '[Gmail]/Starred'
+def number_of_mail_in_folder mbox, folder
   mbox.examine folder
-  uids = mbox.uid_search ['ALL']
+  uids = mbox.uid_search ['all']
   puts "#{uids.size} mails in #{folder}"
   uids.size
 end
 
-def number_mails_on_date_in_folder mbox, args = {}
+def number_of_mail_in_starred mbox
+  number_of_mail_in_folder mbox, folder = '[Gmail]/Starred'
+end
+
+def number_of_mails_in_folder_on_given_date mbox, args = {}
   folder = (args[:folder] || '[Gmail]/All Mail')
   date = (args[:date] || (Date.today - 1))
 
@@ -65,50 +90,57 @@ def number_mails_on_date_in_folder mbox, args = {}
 end
 
 def number_of_mails_sent_yesterday mbox
-  number_mails_on_date_in_folder(
+  number_of_mails_in_folder_on_given_date(
     mbox, :date => (Date.today - 1), :folder => '[Gmail]/Sent Mail'
   )
 end
 
 def number_of_mails_archived_yesterday mbox
-  number_mails_on_date_in_folder(
+  number_of_mails_in_folder_on_given_date(
     mbox, :date => (Date.today - 1), :folder => '[Gmail]/All Mail'
   )
 end
 
 def number_of_mails_deleted_yesterday mbox
-  number_mails_on_date_in_folder(
+  number_of_mails_in_folder_on_given_date(
     mbox, :date => (Date.today - 1), :folder => '[Gmail]/Trash'
   )
 end
 
-# number mails archived yesterday
+# args: login, options: date
+#
+def main args, options = {}
 
-def main args = []
-  c = (YAML.load_file 'accounts.yml') rescue { :account => {} }
-  account = c[:account]
-  uname, password = *args
-  uname && (account[:login] ||= uname)
-  password && (account[:password] ||= password)
-  puts "account: #{account.inspect}"
-  mbox = M.new(account[:login], account[:password])
+  login = args.shift
 
+  options = (Defaults.merge options)
+  date = Date.parse(options[:date])
+
+  # password is prompted, never have that in a config or on the command line!
+  password = get_password
+
+  mbox = M.new login, password
   puts "folders: #{ mbox.folders.map { |f| f.name }.inspect}"
 
-  #['[Gmail]/All Mail', '[Gmail]/Sent Mail', '[Gmail]/Starred'].each do |folder|
-  #  mbox.examine folder
-  #  date = (Date.today - 1 ).strftime "%e-%b-%Y"
-  #  uids = mbox.uid_search(['ON', date])
-  #  puts "#{uids.size} mails in #{folder} on #{date}"
-  #end
+  # number of deleted mails
+  mail_uids = mbox.mails_in_folder_on_given_date '[Gmail]/Trash', date
+  puts "number of deleted mails: #{mail_uids.size}"
 
-  number_of_mails_deleted_yesterday mbox
-  number_of_mails_sent_yesterday mbox
-  number_of_mails_archived_yesterday mbox
-  number_of_mail_in_starred mbox
+  # number of sent mails
+  mail_uids = mbox.mails_in_folder_on_given_date '[Gmail]/Sent Mail', date
+  puts "number of sent mails: #{mail_uids.size}"
+
+  # number of archived mails
+  mail_uids = mbox.mails_in_folder_on_given_date '[Gmail]/All Mail', date
+  puts "number of archived mails: #{mail_uids.size}"
+
+  # total number of starred mails
+  mail_uids = mbox.mails_in_folder_before_given_date('[Gmail]/Starred', date+1)
+  puts "total number of starred mails: #{mail_uids.size}"
 end
 
-main ARGV
+params = Hash.from_argv ARGV
+main params[:args], params
 
 __END__
 p folders.select { |f| f.name == '[Gmail]' }
