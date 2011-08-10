a2836af84cea061875da3fa9c16f6b5b8b9a26b1
dirk luesebrink <dirk.luesebrink@gmail.com>
2011-08-10 13:55:13 +0200

    cronjob parameter combined into bash script

---
+++ gmail-stats-cronjob.sh
@@ -0,0 +1,11 @@
+#!/bin/bash -f
+
+BASE=/home/dirk/proj/gmail-stats
+LOG=${BASE}/gmail-stats-cronjob.log
+
+cd $BASE
+echo "-- $(date)" >> $LOG
+./gmail-stats-client.rb \
+    --db=/home/dirk/proj/gmail-stats/out.yml \
+    --host=127.0.0.1 yesterday \
+   2>&1  >> ${LOG} 
