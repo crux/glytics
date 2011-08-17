fb31dd95bc70828c5c83aea5a8fd1768d4f783a1
dirk luesebrink <dirk.luesebrink@gmail.com>
2011-08-17 15:28:59 +0200

    refactoring. still trying to lear why the cronjob fails

--- gmail-stats-cronjob.sh
+++ gmail-stats-cronjob.sh
@@ -3,9 +3,15 @@
 BASE=/home/dirk/proj/gmail-stats
 LOG=${BASE}/gmail-stats-cronjob.log
 
-cd $BASE
-echo "-- $(date)" >> $LOG
-./gmail-stats-client.rb \
-    --db=/home/dirk/proj/gmail-stats/out.yml \
-    --host=127.0.0.1 yesterday \
-    2>&1  >> ${LOG} 
+CMD="./gmail-stats-client.rb"
+CMD="${CMD} --db=/home/dirk/proj/gmail-stats/out.yml"
+CMD="${CMD} --host=127.0.0.1 yesterday"
+
+function run-report() {
+    cd $BASE
+    echo " -- $(date)"
+    echo "    ${CMD}"
+    ${CMD}
+}
+
+run-report 2>&1  >> ${LOG} 
