feff9c6e46595c47ed2fad7c15787b6d0575eadd
dirk luesebrink <dirk.luesebrink@gmail.com>
2011-08-23 13:22:54 +0200

    namespaced to gytics

--- glytics-cronjob.sh
+++ glytics-cronjob.sh
@@ -3,8 +3,8 @@
 BASE=/home/dirk/proj/gmail-stats
 LOG=${BASE}/gmail-stats-cronjob.log
 
-CMD="./gmail-stats-client.rb"
-CMD="${CMD} --db=/home/dirk/proj/gmail-stats/out.yml"
+CMD="./glytics.rb"
+CMD="${CMD} --db=glytics.yml"
 CMD="${CMD} --host=127.0.0.1 yesterday"
 
 function run-report() {
