bfefb8505e9aff38cebeb7478893064733e1788c
dirk luesebrink <dirk.luesebrink@gmail.com>
2011-09-03 19:43:51 +0200

    alpha/wip: weekly reporting
    aggregates days into weeks for reporting. still need working on the
    right graph labeling, number/levels are not corret. need to work out
    that google charts stuff.

--- glytics-client.rb
+++ glytics-client.rb
@@ -24,7 +24,7 @@ class Model < Hash
   end
 
   def days
-    @days ||= self[:on_date].values.sort {|a, b| a["date"] <=> b["date"]}
+    #
     # thats what an :on_date entry looks like:
     #
     #  "2011-08-24": 
@@ -35,6 +35,26 @@ class Model < Hash
     #    archived: 35
     #    starred: 13
     #
+    @days ||= self[:on_date].values.sort {|a, b| a["date"] <=> b["date"]}
+  end
+
+  def weeks
+    @weeks ||= group_by_weeks(days).values.sort {|a,b| a['date']<=>b['date']}
+  end
+
+  def group_by_weeks days
+    weeks = days.inject({}) do |weeks, record|
+      cweek = Date.parse(record["date"]).cweek
+      week = (weeks[cweek] ||= Hash.new {|h,k| h['cweek'] = cweek})
+      week['date'] = record['date']
+      week['trashed'] += record['trashed']
+      week['sent'] +=  record['sent']
+      week['archived'] += record['archived']
+      week['starred'] = record['starred']
+      weeks
+    end
+    #puts "weeks: #{weeks.inspect}"
+    weeks
   end
 
   def save
@@ -75,7 +95,7 @@ class ChartMaker
     :modelfile => 'glytics.yml', #:filename => 'chart.png', 
     :format => 'url', # || image_tag || file
     :size => '600x200',
-    :line_colors => '4a8c3d,cbd64d,aaaaaa',
+    :line_colors => '4a8c3d,cbd64d,dfc613,cccccc',
   }
 
   def initialize options = {}
@@ -88,28 +108,60 @@ class ChartMaker
     system "open #{@options[:filename]}"
   end
   
-  def days args, options = {}
+  def daily args, options = {}
     options = (@options.merge options)
 
     # render to file when filename is given
     (not options[:filename].nil?) and (options[:format] = 'file')
 
-    dates, sent, archived, trashed = %w(date sent archived trashed).map do |key| 
+    keys = %w(sent archived starred trashed)
+    dates, sent, archived, starred, trashed = (["date"] + keys).map do |key| 
       @model.days.map { |record| record[key] }
     end
 
-    x_label = dates.each_with_index.map {|x,idx| x if idx % 30 == 0}.compact
+    x_label = dates.each_with_index.map {|x,idx| x if idx % 90 == 0}.compact
+    x_label << dates.last
     puts "x_label: #{x_label}"
 
     options.update(
-      :data => [sent, archived, trashed],
-      :legend => ['sent', 'archived', 'trashed'],
-      :title => 'mails per day', 
+      :data => [sent, archived, starred, trashed],
+      :legend => keys,
+      :title => 'mails (daily)',
       :axis_with_labels => ['x', 'y'], # 'date', '# of mails'],
-      #:axis_labels => ['Jan','July','Jan','July','Jan'],
+      :axis_labels => [x_label],
+    )
+    #puts "options: #{options.inspect}"
+
+    chart = Gchart.line(options)
+    puts chart
+  end
+
+  def weekly args, options = {}
+    options = (@options.merge options)
+
+    # render to file when filename is given
+    (not options[:filename].nil?) and (options[:format] = 'file')
+
+    keys = %w(sent archived starred trashed)
+    cweeks, sent, archived, starred, trashed = (['cweek'] + keys).map do |key| 
+      @model.weeks.map { |record| record[key] }
+    end
+
+    n = (cweeks.size.to_f / 4).round
+    x_label = cweeks.each_with_index.map {|x,idx| x if idx % n == 0}.compact
+    x_label << cweeks.last
+    #puts "x_label: #{x_label}"
+
+    options.update(
+      :legend => keys,
+      :title => 'mails (weekly)',
+      :axis_with_labels => ['x', 'y', 'r', 't'], # 'date', '# of mails'],
       :axis_labels => [x_label],
     )
     puts "options: #{options.inspect}"
+
+    # inject data AFTER we dumped options to stdout
+    options.update(:data => [sent, archived, starred, trashed])
 
     chart = Gchart.line(options)
     puts chart
@@ -123,8 +175,12 @@ Applix.main(ARGV, Defaults) do
       @cm = ChartMaker.new options
     end
 
-    handle(:days) do |*args, options|
-      @cm.days args, options
+    handle(:daily) do |*args, options|
+      @cm.daily args, options
+    end
+
+    handle(:weekly) do |*args, options|
+      @cm.weekly args, options
     end
 
     epilog do |records, *args, options|
